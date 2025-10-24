af2fae33eb72d7f44dba11757c9295bde8d04e9e
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-10-24 15:46:30 +0300

    feat: add cron job

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,10 +1,8 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-admin.json";
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        credential: admin.credential.applicationDefault(),
     });
 }
 
