86ebbda1d1c6dc4aaab7ff44f48aca99c17a7dab
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-11-20 13:36:42 +0300

    feat: update cron job to GET request

--- src/app/api/cron/generate-study-plans/route.ts
+++ src/app/api/cron/generate-study-plans/route.ts
@@ -1,7 +1,7 @@
 import {NextRequest} from "next/server";
 import {generateDailyStudyPlans} from "@/lib/services/studyPlanGenerator";
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
     console.log(req.body);
     await generateDailyStudyPlans()
     return new Response('Study plans generated', {status: 200});
